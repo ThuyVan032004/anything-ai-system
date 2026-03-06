87352aeefea82167bdfafb0976adf70849f36c38
ThuyVan032004 <dovan032004@gmail.com>
2026-03-06 19:58:20 +0700

    update

--- shared/src/pipeline/models/dag_docker_task_config_model.py
+++ shared/src/pipeline/models/dag_docker_task_config_model.py
@@ -7,7 +7,7 @@ class DagDockerTaskModel(BaseModel):
     api_version: Optional[str] = "auto"
     auto_remove: Optional[str] = "force"
     command: str
-    docker_url: Optional[str] = "tcp://airflow-docker-proxy:2375"
+    docker_url: Optional[str] = "tcp://airflow-docker:2375"
     network_mode: str = "bridge"
     mount_tmp_dir: Optional[bool] = False
     environment: Optional[Dict[str, str]] = None
